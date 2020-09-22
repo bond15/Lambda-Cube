cf6dcc74de0b60e2df5ce38d8ea1f3e186737590
bond15 <bond15@purdue.edu>
2020-09-22 09:28:58 -0700

    add stlc type checker

--- src/STLC/DeBruijn.hs
+++ src/STLC/DeBruijn.hs
@@ -1,19 +1,64 @@
 module STLC.DeBruijn where
-
+import Data.Set
 -- Church style typing: Types are part of the term
 -- Curry style typing: Terms are UTLC, Types are "checked"
 
+-- Note: without base types, STLC is uninhabited
 data Type =
       Unit 
-    | Bool
     | Arr Type Type  -- right associative
-      deriving (Show)
+      deriving (Show, Eq, Ord) --naive type equality(strict representation)
 
 data Term = 
       TmVar Int
     | TmAbs Type Term
     | TmApp Term Term
-    | T
-    | True
-    | False
+    | T deriving (Show, Eq, Ord) --naive term equality(strict representation)
+
+-- Not enforcing substructural rules like 
+-- Weakening, exchange, ...etc
+type Ctxt = Set (Term, Type)
+
+isVal :: Term -> Bool
+isVal (TmAbs _ _) = True
+isVal _ = False
+
+-- alternative, define a getType operation instead of a check operation
+-- infer vs check , idea behind bidirectional type checking?
+typeCheck :: Ctxt -> Term -> Type -> Bool
+{- 
+
+-------------------
+ Gamma |-  T : Unit
+-}
+typeCheck ctx T Unit = True
+
+{- 
+  x : T in Gamma
+-------------------
+ Gamma |- x : T
+-}
+typeCheck ctx (TmVar n) ty = member ((TmVar n), ty) ctx
+
+{-
+Gamma, x : T1 |- t2 : T2
+-------------------------------
+Gamma |- \x : T1. t2 : T1 -> T2
+-}
+typeCheck ctx (TmAbs ty1 t2) (Arr ty1' ty2) | ty1 == ty1' = typeCheck (insert (TmVar (size ctx),ty1) ctx) t2 ty2
+typeCheck ctx (TmAbs ty1 t2) (Arr ty1' ty2) | ty1 /= ty1' = False
+
+{--
+Gamma |- t1 : T1 -> T2    Gamma |- t2 : T1
+-------------------------------------------
+Gamma |- t1 t2 : T2
+-}
+-- have to peek at t1 here, must be a lambda abstraction
+typeCheck ctx (TmApp (TmAbs ty1 b) t2) ty2 = let r1 = typeCheck ctx (TmAbs ty1 b) (Arr ty1 ty2) 
+                                                 r2 = typeCheck ctx t2 ty1
+                                              in r1 && r2
+typeCheck ctx (TmApp t1 t2) ty2 = False
+
+typeCheck _ _ _ = False
+
 
